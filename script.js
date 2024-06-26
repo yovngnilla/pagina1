70bc61c3148d7a8ab419547ce7891505971bdc6f
yovngnilla <rodriguezbonilla.landonjonathan@cetis153.edu.mx>
2024-06-26 15:52:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// Swiper 1
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
+
+// Swiper 2
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
+    }
+});
+
+// Tabs Input
+let tabInputs = document.querySelectorAll(".tabInput");
+
+tabInputs.forEach(function(input) {
+
+    input.addEventListener('change', function() {
+        let id = input.getAttribute('aria-valuemax');
+        let thisSwiper = document.getElementById('swiper' + id);
+        thisSwiper.swiper.update();
+    })
+});
